507040f2e43b2e13193f8de0116dfc38f9e703dd
MitchellStokes <Mitchell.Stokes@Domino-UK.com>
2015-08-06 15:06:06 +0100

    Committing to my repo

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## We will store the matrix in variable x
+## We will store the inverse of the matrix in variable xInverse
+## Comments have been scattered throughout the function
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function will create a matrix 'x' before creating an inverse matrix variable 'xInverse'.
+## The function then sets inversion functions and applies them to the matrix variable
 
+makeCacheMatrix <- function(x=matrix()) {
+  
+  xInverse <- NULL ##Initialise the inverse matrix variable
+  
+  set <- function(y) {
+    
+    x <<- y
+    xInverse <<- NULL ##Initialise the inverse matrix variable in this environment
+    
+  }
+  
+  get <- function() x ## Store/Return the matrix to the get variable
+  setInverse <- function(inv) xInverse <<- inv ## Create the inversion
+  getInverse <- function() xInverse ## Store/Return the inverse matrix to the getInverse variable
+  list(set=set,get=get,setInverse=setInverse,getInverse=getInverse) ## Store the variables the matrix to be called later
+  
 }
 
 
-## Write a short comment describing this function
+## This function will attempt to find the variable xInverse in the previous function
+## If this variable does exist, the function will return the Inverse Matrix variable 'xInverse'
+## If this variable does not exist, that means the 'makeCacheMatrix' function has not been run
+## In this case, the function will call the previously defined functions within the 
+## 'makeCacheMatrix' function and solve the matrix itself, before returning it
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+  
+  xInverse <- x$getInverse() ## Attempt to retrieve the inverse matrix and store it in variable XInverse
+  if(!is.null(xInverse)) { ## Check if xInverse is not null, if so exit and return xInverse, if not continue
+    message("Retrieving the Inverse Matrix")
+    return(xInverse)
+  }
+  getAgain<-x$get() ## Get the matrix again
+  xInverse<-solve(getAgain,...) ## Solve the matrix and store to variable xInverse
+  x$setInverse(xInverse) ## Get inverse of xInverse using function defined above
+  xInverse ## Return the inverse matrix
 }
